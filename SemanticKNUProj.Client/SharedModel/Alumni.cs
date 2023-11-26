877c100b3c29ed3b850216b0c74b5c34a8ed52ef
dante380 <steamhater2@gmail.com>
2023-11-27 01:44:11 +0200

    Resolved PUT request

---
+++ SemanticKNUProj.Client/SharedModel/Alumni.cs
@@ -0,0 +1,9 @@
+﻿namespace SemanticKNUProj.Client.SharedModel
+{
+    public class Alumni
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Link { get; set; }
+    }
+}
